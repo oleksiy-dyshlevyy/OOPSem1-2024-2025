10fe4e025b80e428bac85f05519fccde66f2b75c
oleksiy-dyshlevyy <oleksiy.dyshlevyy@gmail.com>
2025-03-09 09:46:40 +0200

    Examples about Properties, Inexers and Inheritance were added

---
+++ Topic6/Updated files from OOPParametersOperators/Program.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPParametersOperators
+{
+    class Program
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static string Add(string str1, string str2)
+        {
+            return str1 + str2;
+        }
+
+        public static int Add (params int[]values)
+        {
+            int tmp = 0;
+            for (int i = 0; i < values.Length; i++)
+                tmp += values[i];
+
+            return tmp;
+        }
+
+        public static void Method(ref Sqr p, ref int a)
+        {
+          a = 100;
+            Sqr o = new Sqr();
+                  o.Side1 = 10000;
+            p = o;
+             p.Side1 = 10;
+        }
+
+        public static Sqr Method1(int a, int b = 0, params int[] c)
+        {
+            Sqr s = new Sqr();
+            //          s.side = b;
+            a = 100;
+            return s;
+        }
+
+        public static void Method2(int a, int b = 0, params int[] c)
+        {
+
+            a = 1000;
+            //  b = 500;
+            
+            c[0] = 4;
+            c[1] = 7;
+            c[10] = 8;
+        }
+       
+        public static void Method3(Sqr sqr)
+        {
+            sqr.Side1 = 1000;
+            sqr = new Sqr();
+            sqr.Side1 = 500;
+
+            Sqr sqr5 = new Sqr();
+            sqr5.Side1 = 1500;
+            sqr = sqr5;
+
+        }
+
+        public static void Method4(int a)
+        {
+            a = 10;
+        }
+        static void Main(string[] args)
+        {
+           // object o = new object();    
+
+            int tmp = Add(1,2);
+            string tmpStr = Add("Hello ", "World!");
+
+            int[] arr = {1,2,3,4,5};
+            tmp = Add(arr);
+            tmp = Add(1, 2, 3);
+            tmp = Add(1, 5, 18, 45);
+            
+            int result = 5;
+
+            Method4(result);
+            Sqr sqr1 = new Sqr();
+            sqr1.Side1 = 1000;
+           // sqr1.Side2 = 500;
+            Sqr sqr2 = new Sqr(5);
+
+        //    sqr1.b = true;
+
+            sqr1.Side1 = 3;
+            double r = sqr1.Side1;
+
+            r = sqr1.Side;
+            sqr1.Side = -5;
+
+            int side1 = sqr1[0, 0];
+            sqr1[1, 0] = 5;
+
+           // Sqr sqr5 = new Sqr { b = true, c = 10 };
+
+            Method3(sqr1);
+
+           // int t;
+            Method(ref sqr1, ref result);
+            Method2(1,2,result);
+
+            sqr2 = Method1(a: result, b: result * 2, c: new int[] { result * 3, result * 4 });
+           
+            Method2(result, result * 2, result * 3, result * 4);
+
+            Sqr sqr3 = sqr1 + sqr2;
+            sqr3 = sqr1 + 5;
+            if ((sqr3 <  sqr2) && !sqr1)
+                sqr2 = 4 + sqr3;
+            sqr2++;
+
+            bool b = true;
+
+      
+      
+
+            ++sqr1;
+
+            bool bb = !sqr2;
+
+            sqr1.Side = 5;
+            Console.WriteLine(sqr1.Side);
+
+            // System.Object o = sqr3 as object;
+
+            if (sqr1 != sqr2)
+            { }
+
+            Hour hour = new Hour(4);
+            int h = hour;
+            h++;
+            hour = (Hour)h;
+
+            Employee employee1 = new Employee();
+            Employee employee2 = new Employee()
+            {
+                firstName = "Ivan",
+                lastName = "Ivanov",
+                birthDate = new Date(),
+                hireDate = new Date(20, 10, 2017)
+                //{
+                //    day = 20,
+                //    month = 10,
+                //    year = 2017
+                //},
+            };
+        }
+    }
+}
---
+++ Topic6/Updated files from OOPParametersOperators/Sqr.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPParametersOperators
+{
+    public class Sqr
+    {
+        int side;
+        public bool b;
+        public int c;
+        string description;
+
+        public int Side
+        {
+            get  
+            {
+                side++;
+                return side; 
+            }
+            set
+            {
+                if (value > 0)
+                    side = value;
+             //   Console.WriteLine("sdfdsf");
+            }
+        }
+        public double Side1 { get; set; }// = 2.5;
+        public double Side2 { get; }
+
+       
+        int[,] sides = new int[4,4];
+        int[] sides1 = new int[4];
+
+        public int this[int i]
+        {
+            get
+            {
+                return sides1[i];
+            }
+            set
+            {
+                sides1[i] = value;
+            }
+        }
+        public int this[int i, int j]
+        {
+            get 
+            {
+                return sides[i,j];
+            }
+            set 
+            {
+                sides[i,j] = value;
+            }
+        }
+
+
+        // public string Description => description;
+
+        //double side2;
+        //public double GetSide2()
+        //{
+        //    return side2;
+        //}
+
+        //public void SetSide2(double newSide)
+        //{
+        //    side2 = newSide;
+        //}
+
+        public Sqr()
+        {
+            side = 1;
+           // Side2 = 10;
+        }
+
+        public Sqr(int side)
+        {
+            this.side = side;
+        }
+
+
+        public Sqr(int parside, Boolean f)
+        {
+            if (f == true)
+                side = parside;
+            else
+                side = parside * 2;
+        }
+
+        public Sqr(Sqr obj)
+        {
+            side = obj.side;
+        }
+
+        public int P()
+        {
+            return side * 4;
+
+        }
+
+        public int S()
+        {
+            return side * side;
+        }
+
+        public int Get()
+        {
+            return side;
+        }
+
+        public void Set(int parside)
+        {
+            if (parside > 0)
+            {
+                side = parside;
+            }
+        }
+
+      
+        public static Sqr operator +(Sqr s1, Sqr s2)
+        {
+            Sqr s3 = new Sqr();
+            s3.side = s1.side + s2.side;
+            return s3;
+        }
+
+        public static Sqr operator +(Sqr s1, int i2)
+        {
+            Sqr s3 = new Sqr();
+            s3.side = s1.side + i2;
+            return s3;
+        }
+
+        public static Sqr operator +(int i1, Sqr s2)
+        {
+            Sqr s3 = new Sqr();
+            s3.side = i1 + s2.side;
+            return s3;
+        }
+
+        public static bool operator >(Sqr s1, Sqr s2)
+        {
+            if (s1.side > s2.side)
+                return true;
+            else
+                return false;
+        }
+
+        public static bool operator <(Sqr s1, Sqr s2)
+        {
+            if (s1.side < s2.side)
+                return true;
+            else
+                return false;
+        }
+
+        public static bool operator !(Sqr s2)
+        {
+            if (0 < s2.side)
+                return true;
+            else
+                return false;
+        }
+
+        public static bool operator true(Sqr s2)
+        {
+            if (0 == s2.side)
+                return true;
+            else
+                return false;
+        }
+
+        public static bool operator false(Sqr s2)
+        {
+            if (0 != s2.side)
+                return true;
+            else
+                return false;
+        }
+
+        public static Sqr operator ++(Sqr s)
+        {
+            s.side++;
+            return s;
+        }
+    }
+}
